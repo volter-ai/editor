c9551e8c6f181c2816329ac15702eb9f0eabf368
yueranyuan <yueranyuan@users.noreply.github.com>
2026-09-23 06:00:24 -0400

    Restore native sidebar selection on packaged cold starts

--- packages/editor-core/workbench/src/vgai.contribution.ts
+++ packages/editor-core/workbench/src/vgai.contribution.ts
@@ -21,6 +21,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import './media/vgai-parts.css';
+import './vgaiSidebarRestore.js';
 import { attachDocumentView, installDocumentViewSink, VgaiDocumentViews } from './vgaiDocumentViews.js';
 import { $, Dimension } from '../../../../base/browser/dom.js';
 import { mainWindow } from '../../../../base/browser/window.js';
